0a78eb23bde0dcc02b4e64f7e6b88b77392cb9a0
Tano <github@tano.asia>
2026-05-21 20:36:43 +0800

    feat: 修改部分内容

--- src/plugins/basic/__init__.py
+++ src/plugins/basic/__init__.py
@@ -1,5 +1,3 @@
-import random
-
 from nonebot import get_plugin_config, on_command
 from nonebot.adapters.qq import MessageEvent
 from nonebot.adapters.qq.event import (
@@ -13,37 +11,11 @@ from .config import Config
 __plugin_meta__ = PluginMetadata(
     name="basic",
     description="基础命令插件",
-    usage="发送 /ping, /hello, /help 等命令",
+    usage="发送 /help 等命令",
     config=Config,
 )
 
 config = get_plugin_config(Config)
-
-# Ping 命令
-ping_cmd = on_command("ping", aliases={"ping"}, priority=10, block=True)
-
-
-@ping_cmd.handle()
-async def handle_ping() -> None:
-    await ping_cmd.finish("pong")
-
-
-# 问候命令
-hello_cmd = on_command("hello", aliases={"你好"}, priority=10, block=True)
-
-HELLO_RESPONSES = [
-    "你好！",
-    "嗨！",
-    "Hello！",
-    "你好呀！",
-    "嗨嗨！",
-]
-
-
-@hello_cmd.handle()
-async def handle_hello() -> None:
-    response = random.choice(HELLO_RESPONSES)
-    await hello_cmd.finish(response)
 
 
 # 帮助命令
@@ -52,16 +24,16 @@ help_cmd = on_command("help", aliases={"帮助"}, priority=10, block=True)
 HELP_TEXT = """可用命令列表：
 
 基础命令：
-/ping - 测试机器人响应
-/hello 或 /你好 - 打招呼
-/help 或 /帮助 - 显示此帮助信息
-/id 或 /getid - 查询用户ID和群组ID
+/help 显示此帮助信息
 
-娱乐命令：
-/random <最小值> <最大值> - 生成随机数
-/joke 或 /笑话 - 听个笑话
-/dice 或 /骰子 - 掷骰子
-/guess - 猜数字游戏"""
+BanG Dream：
+/bangdream 角色知识问答
+
+演出查询：
+/live 或 /未来演出 - 查询近期演出信息
+/live <乐队名> - 按乐队筛选演出
+/刷新演出 - 手动刷新演出数据
+"""
 
 
 @help_cmd.handle()
