a993392e0824bafb828c9a99ae968f094b98acf6
Tano <github@tano.asia>
2026-09-15 21:23:40 +0800

    refactor: 移除演示插件并补齐 QQ 适配器所需的驱动与依赖

--- src/plugins/bangdream/config.py
+++
@@ -1,5 +0,0 @@
-from pydantic import BaseModel
-
-
-class Config(BaseModel):
-    """BanG Dream 问答插件配置"""
--- src/plugins/proactive/config.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-
-
-class Config(BaseModel):
-    """主动消息测试插件配置"""
-
-    proactive_group_id: str = ""
